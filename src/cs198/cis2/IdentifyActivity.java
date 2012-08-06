f2fe66222fb10eaba1055523e3dfa343c3b5c333
norvinyecla <yving07@yahoo.com>
2012-08-06 15:23:37 +0800

    started files

---
+++ src/cs198/cis2/IdentifyActivity.java
@@ -0,0 +1,17 @@
+package cs198.cis2;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class IdentifyActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.identify);
+    }
+
+    
+}
+
---
+++ src/cs198/cis2/LaunchActivity.java
@@ -0,0 +1,28 @@
+package cs198.cis2;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class LaunchActivity extends Activity {
+	private Button continueButton;
+	
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launch);
+        continueButton =  (Button) findViewById(R.id.button1);
+        this.continueButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            	Intent myIntent = new Intent(LaunchActivity.this, IdentifyActivity.class);
+            	LaunchActivity.this.startActivity(myIntent);
+            }
+          });
+    }
+
+    
+}
